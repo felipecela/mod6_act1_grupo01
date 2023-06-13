be74968d80c2a663529aea2df127166bb4411682
Naza1512 <therazan@gmail.com>
2023-06-13 23:20:41 +0200

    Modificación del titulo noticias de la pagina noti

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -53,7 +53,7 @@ const NewsPage = () => {
 
     return (
         <div className={styles.container}>
-            <PageTitle text={'News'} />
+            <PageTitle text={'Últimas noticias'} />
             <div className={styles.formWrapper}>
                 <SearchForm onSubmit={searchNewsByInputWord} onClear={onClear} />
             </div>
